751eec58fc02e0656d5901af2952ac684442904b
Jeonseoghyeon <jsh28592@gmail.com>
2020-04-03 15:04:08 +0900

    최적화 1

---
+++ Solving Club/휴강기간/미로의거리_창완형수정.py
@@ -0,0 +1,39 @@
+import sys
+sys.stdin = open("미로의거리_input.txt","r")
+
+from collections import deque
+
+dx,dy = [0,0,1,-1],[1,-1,0,0]
+
+def bfs(arr,y,x):
+    global visited
+    visited[y][x] = 1
+    dq.append([y,x,0])
+    while dq:
+        node_y, node_x,cnt = dq.popleft()
+        for i in range(4):
+            ny = node_y + dy[i]
+            nx = node_x + dx[i]
+            if 0<=nx<N and 0<=ny<N and visited[ny][nx]==0:
+                if arr[ny][nx] == '0':
+                    visited[ny][nx] = 1
+                    dq.append([ny,nx,cnt+1])
+                elif arr[ny][nx] == '3':
+                    return cnt
+    return 0        
+
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    arr = [list(input()) for ARR in range(N)]
+    visited = [[0]*N for VISITED in range(N)]
+
+    for y in range(N):
+        for x in range(N):
+            if arr[y][x] == '2':
+                start_y = y
+                start_x = x
+    dq = deque()
+    result = bfs(arr,start_y,start_x)
+    
+    print("#{} {}".format(tc,result))
